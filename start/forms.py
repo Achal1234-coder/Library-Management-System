1b8805e1e51f44bcad5af49260c5607eaaa1b2c0
Achal Gupta <achal.gupta.16@mountblue.tech>
2021-06-14 13:01:15 +0000

    Update forms.py

--- start/forms.py
+++ start/forms.py
@@ -1,34 +1,42 @@
 from django import forms
-from .models import Registration_Student
+from .models import RegistrationStudent
 
 
-class Register_student_form(forms.Form):
-    Student_Name = forms.CharField(max_length=30)
-    Student_Id = forms.IntegerField()
-    Password = forms.CharField(widget=forms.PasswordInput())
-    Confirm_Password = forms.CharField(widget=forms.PasswordInput())
+class RegisterStudentForm(forms.Form):
+    ''' Make a form named RegisterStudentForm '''
 
-    def clean_Student_Name(self):
-        name = self.cleaned_data.get('Student_Name')
+    student_name = forms.CharField(max_length=30)
+    student_id = forms.IntegerField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
+    def clean_student_name(self):
+        ''' this function check the validation of student_name field '''
+
+        name = self.cleaned_data.get('student_name')
         if len(name) < 3 or name is None:
             raise forms.ValidationError('Not valid')
         else:
             return name
 
-    def clean_Password(self):
-        password1 = self.cleaned_data.get('Password')
-        if len(password1) < 5:
+    def clean_password(self):
+        ''' this function check the validation of password field '''
+
+        password = self.cleaned_data.get('password')
+        if len(password) < 5:
             raise forms.ValidationError('Password to short')
         else:
-            return password1
+            return password
 
 
-class Login_student_form(forms.ModelForm):
-    Password = forms.CharField(max_length=20, widget=forms.PasswordInput())
+class LoginStudentForm(forms.ModelForm):
+    ''' Make a form named RegisterStudentForm '''
+    
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput())
 
     class Meta:
-        model = Registration_Student
+        model = RegistrationStudent
         fields = [
-            'Student_Id',
-            'Password'
+            'student_id',
+            'password'
         ]
