1766400f5a02406993738413bd128f221ec64334
Achal Gupta <achal.gupta.16@mountblue.tech>
2021-06-14 12:52:14 +0000

    Update models.py

--- start/models.py
+++ start/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-''' Make a model named RegistrationStudent '''
 class RegistrationStudent(models.Model):
+    ''' Make a model named RegistrationStudent '''
+
     student_Name = models.CharField(max_length=30)
     student_Id = models.IntegerField()
     password = models.CharField(max_length=20)
