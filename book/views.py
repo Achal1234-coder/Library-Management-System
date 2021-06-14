7521273946b033c690ff63ede9c458dac3baea14
Achal Gupta <achal.gupta.16@mountblue.tech>
2021-06-14 13:39:33 +0000

    Update views.py

--- book/views.py
+++ book/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .forms import Book_forms
+from .forms import BookForms
 from django.contrib import messages
 from .models import Books
 
 
 def create_book_view(request):
-    form = Book_forms(request.POST or None, request.FILES)
+    ''' this function help to create a new book '''
+
+    form = BookForms(request.POST or None, request.FILES)
     if form.is_valid():
         form.save()
         messages.success(request, 'New Book added successfully')
@@ -14,5 +16,7 @@ def create_book_view(request):
 
 
 def read_book_view(request):
-    Book_obj = Books.objects.all()
-    return render(request, 'book/read.html', {'Book_obj': Book_obj})
+    ''' this function help to read all books '''
+    
+    book_obj = Books.objects.all()
+    return render(request, 'book/read.html', {'Book_obj': book_obj})
