bd6d200a2875e23b7136d8147de2c05d5b766e33
Achal Gupta <achal.gupta.16@mountblue.tech>
2021-06-14 13:20:57 +0000

    Update views.py

--- start/views.py
+++ start/views.py
@@ -1,46 +1,54 @@
 from django.shortcuts import render
-from .forms import Register_student_form, Login_student_form
-from .models import Registration_Student
+from .forms import RegisterStudentForm, LoginStudentForm
+from .models import RegistrationStudent
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 
 
 def home_view(request):
+    ''' this function render the home.html '''
+
     return render(request, 'start/home.html', {})
 
 
 def register_student_view(request):
-    form = Register_student_form(request.POST or None)
+    ''' this function registered a student '''
+
+    form = RegisterStudentForm(request.POST or None)
     if form.is_valid():
-        try:
-            Id = request.POST.get('Student_Id')
-            Registration_Student.objects.get(Student_Id=Id)
+        try:   # if id exist it return message error
+            id = request.POST.get('student_id')
+            RegistrationStudent.objects.get(student_id=id)
             messages.error(request, 'You are already Registered')
         except ObjectDoesNotExist:
-            password1 = request.POST.get('Password')
-            password2 = request.POST.get('Confirm_Password')
-            if password1 != password2:
+            # if try through exception then it check if and else block
+
+            password = request.POST.get('password')
+            confirm_password = request.POST.get('confirm_password')
+            if password != confirm_password:
                 messages.error(request, 'Password not match')
             else:
-                Obj = Registration_Student.objects.create(
-                    Student_Name=request.POST.get('Student_Name'),
-                    Student_Id=request.POST.get('Student_Id'),
-                    Password=request.POST.get('Password')
+                obj = RegistrationStudent.objects.create(
+                    student_name=request.POST.get('student_name'),
+                    student_id=request.POST.get('student_id'),
+                    password=request.POST.get('password')
                     )
-                Obj.save()
+                obj.save()
                 messages.success(request, 'Registration Successful')
-                form = Register_student_form()
+                form = RegisterStudentForm()
     return render(request, 'start/register.html', {'form': form})
 
 
 def login_student_view(request):
-    form = Login_student_form(request.POST or None)
+    ''' this function check login status of student '''
+
+    form = LoginStudentForm(request.POST or None)
     if form.is_valid():
-        Id = request.POST.get('Student_Id')
-        Obj = Registration_Student.objects.get(Student_Id=Id)
-        if Obj.Password != request.POST.get('Password'):
+        id = request.POST.get('student_id')
+        obj = RegistrationStudent.objects.get(student_id=id)
+        if obj.password != request.POST.get('password'):
             messages.error(request, 'Password Not Matched')
         else:
             print('Successful')
-            form = Login_student_form()
+            form = LoginStudentForm()
     return render(request, 'start/login.html', {'form': form})
