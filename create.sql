5a36f333d1a96b0c608826fd81602a640a120cab
Achal Gupta <achal.gupta.16@mountblue.tech>
2021-06-19 07:02:20 +0000

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,3 +1,3 @@
 create user achal with password 'achal"123';
-create database Django-Project;
-grant all privileges on database Django-Project to achal;
+create database Library;
+grant all privileges on database Library to achal;
